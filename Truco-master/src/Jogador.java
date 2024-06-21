6e04e3e607b8e956e8d1d3b2595b7221b7302278
DevJhonaN <168478697+DevJhonaN@users.noreply.github.com>
2024-06-21 00:18:56 -0300

    Primeiro Commit

---
+++ Truco-master/src/Jogador.java
@@ -0,0 +1,68 @@
+package src;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Jogador {
+
+    public List<Carta> cartas;
+
+    public String nome;
+
+    public Jogador(String nome) {
+        this.cartas = new ArrayList<>();
+        this.nome = nome;
+    }
+
+    public Carta jogada(Carta cartaNaMesa){
+        // Se não há carta na mesa, o jogador é o primeiro a jogar
+if (cartaNaMesa == null) {
+    // Joga a carta de menor valor na primeira rodada
+    return cartaDeMenorValor();
+} else {
+    // Tenta jogar uma carta que ganhe da carta na mesa
+    Carta cartaParaGanhar = cartaParaGanhar(cartaNaMesa);
+    if (cartaParaGanhar != null) {
+        return cartaParaGanhar;
+    } else {
+        // Se não pode ganhar, joga a carta de menor valor
+        return cartaDeMenorValor();
+     }  
+  }
+}
+
+
+// Função para encontrar a carta de menor valor
+private Carta cartaDeMenorValor() {
+    Carta menorCarta = cartas.get(0);
+    for (Carta carta : cartas) {
+        if (carta.valor < menorCarta.valor) {
+            menorCarta = carta;
+        }
+    }
+    cartas.remove(menorCarta);
+    return menorCarta;
+}
+
+// Função para encontrar uma carta que ganhe da carta na mesa
+private Carta cartaParaGanhar(Carta cartaNaMesa) {
+    Carta melhorCarta = null;
+    for (Carta carta : cartas) {
+        if (carta.valor > cartaNaMesa.valor) {
+            if (melhorCarta == null || carta.valor < melhorCarta.valor) {
+                melhorCarta = carta;
+            }
+        }
+    }
+    if (melhorCarta != null) {
+        cartas.remove(melhorCarta);
+    }
+    return melhorCarta;
+}
+    public void exibeCartas(){
+        cartas.forEach(c -> {
+            System.out.print(c + " ");
+        });
+        System.out.println();
+    }
+}
